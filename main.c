05e646c02f06d71683a1667cf1c364cc8a44ee7a
Omega-Dynasty <jeremyhugues59@gmail.com>
2026-04-24 14:16:19 +0200

    Integration de la construction de l'arbre dans le main

--- main.c
+++ main.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[]) {
 
     int freq_table[256] = {0};
     count_frequencies(file, freq_table); // Fonction de ta branche feat-analyse
-    
-    // ... après count_frequencies(file, freq_table);
-
     printf("Resultats de l'analyse :\n");
     for (int i = 0; i < 256; i++) {
         if (freq_table[i] > 0) {
             printf("Caractere '%c' (code %d) : %d fois\n", i, i, freq_table[i]);
         }
     }
+    Node* root = build_huffman_tree(freq_table);
+    printf("Arbre de Huffman construit avec succes.\n");
+    free_tree(root);
 }
