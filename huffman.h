2fd033e636d7e8134f62a9b0d418b808a3cfa3e7
Omega-Dynasty <jeremyhugues59@gmail.com>
2026-04-24 14:20:11 +0200

    Correction des declarations implicites dans le header

--- huffman.h
+++ huffman.h
@@ -1,10 +1,21 @@
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
+// Définition de la structure
 typedef struct Node {
     char character;
     int frequency;
     struct Node *left, *right;
 } Node;
 
+// Déclarations des fonctions pour que main.c les connaisse
 void count_frequencies(FILE *file, int *freq_table);
+Node* create_node(char c, int freq);
+Node* build_huffman_tree(int *freq_table);
+void generate_codes(Node* root, int* code, int top);
+void free_tree(Node* root);
+
+#endif
